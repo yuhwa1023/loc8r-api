efee1a77f2a817dc2ceb9fc24167376f56fcf5a8
yuhwa1023 <baeromi23@gmail.com>
2024-11-03 02:46:04 +0900

    Add files via upload

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,7 @@ const apiOptions = {
   server: 'http://localhost:3000'
 };
 if(process.env.NODE_ENV === 'production'){
-  apiOptions.server  = 'https://yourapi.com';
+  apiOptions.server  = 'https://loc8r-api-0sv9.onrender.com';
 }
 
 const homelist = (req, res) => {
